44b58ccbdda2b32ae1c31a5ae68dbcf2a46140f7
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-09-08 15:54:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002927765158"))
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://kmjc4lad8c_db_user:jEXRvn3Dt1E5fOvz@cluster0.bbypg1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-ADMINS = set(str(x) for x in environ.get("ADMINS", "6725874739").split())
+ADMINS = set(str(x) for x in environ.get("ADMINS", "6725874739 1018033649").split())
 AUTO_DELETE = environ.get("AUTO_DELETE", "1") == "1"
 AUTO_DELETE_SECOND = int(environ.get("AUTO_DELETE_SECOND", "300"))
